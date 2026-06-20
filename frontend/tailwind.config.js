704e594cce8b712872182f0a1e906c0639bfbf23
GrahamSH <grahamshllk@gmail.com>
2026-06-20 14:24:33 -0400

    upgrade tailwind and daisyui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontSize: {
@@ -25,6 +23,4 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui"), require("@tailwindcss/typography")],
-  daisyui: { themes: false },
 };
